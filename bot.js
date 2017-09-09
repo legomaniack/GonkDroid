41111c8d8994571e1292e650e7de186539197fe2
legomaniack <ng31415@gmail.com>
2017-09-09 01:13:59 -0500

    Added disconnecting from server on close

--- bot.js
+++ bot.js
@@ -174,4 +174,24 @@ function playFile(message, file) {
     }).catch(err => console.log(err));
 }
 
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    if (options.cleanup) {
+        console.log('Disconnected');
+        client.destroy();
+    }
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {cleanup:true, exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
 client.login(auth.token);
